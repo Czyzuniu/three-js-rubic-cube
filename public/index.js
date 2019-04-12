4977b450ce00d6a5cc8b80ed77ff4e14e54daecd
unknown <kolpak.conrad@gmail.com>
2019-04-12 09:10:34 +0100

    first commit

---
+++ public/index.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+  const scene = new THREE.Scene();
+  let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+  let renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+
+  let geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  let cube = new THREE.Mesh( geometry, material );
+  scene.add( cube );
+
+  camera.position.z = 5;
+
+
+
+  function animate() {
+    requestAnimationFrame( animate );
+    renderer.render( scene, camera );
+  }
+  animate();
+
+})
